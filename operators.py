382e295c2089f3ecfbe13abc83a1efc5776eeff4
Ori <odmark2023@gmail.com>
2025-03-17 13:52:39 +0200

    "operators"

---
+++ operators.py
@@ -0,0 +1,21 @@
+from re import Scanner
+
+
+def my_function(first_name):
+    print(first_name)
+
+my_function("ori")
+
+def calculate_bmi(weight , height):
+    return weight / (height ** 2)
+
+print(calculate_bmi(70 , 1.74))
+
+def perimeter_rectangle(length , height):
+    return 2 * (length + height)
+
+def area_rectangle(length , height):
+    return length * height
+
+
+
