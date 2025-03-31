a984ae1d42522f0073c404ecfef45d39d581404e
Ori <odmark2023@gmail.com>
2025-03-31 09:38:40 +0300

    "add function leap year"

--- operators.py
+++ operators.py
@@ -275,3 +275,13 @@ def title():
     print(avi.title())
 
 #title()
+
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return True
+    elif year % 4 == 0:
+        return True
+    else:
+        print(f"The year {year} is not leap ")
