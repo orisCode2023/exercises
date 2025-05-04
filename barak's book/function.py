ea583ed1d787764e5f69f75679b52bbcba5a7d51
Ori <odmark2023@gmail.com>
2025-05-04 11:52:01 +0300

    function from Barak Gonenn book

---
+++ barak's book/function.py
@@ -0,0 +1,15 @@
+def speak():
+    greet = "hi"
+    print(f"{greet} {name}")
+
+name = "boy"
+#speak()
+def check(num1):
+    if True:
+        print("OK")
+    else:
+        bla(blabla)
+#check(1)
+
+a = 24
+print(id(a))
